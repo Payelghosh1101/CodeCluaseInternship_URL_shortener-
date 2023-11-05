a39299ef87b57706077fad7cacc453ecbd68e859
Payelghosh1101 <140962607+Payelghosh1101@users.noreply.github.com>
2023-11-05 12:24:58 +0530

    Update URL_shortener.py

--- URL_shortener.py
+++ URL_shortener.py
@@ -5,7 +5,7 @@ url = input("\nEnter the url : ")
 
 def shortenurl(url):
     s = pyshorteners.Shortener()
-    print("\nshot url : ",s.tinyurl.short(url),"\n")
+    print("\nshort url : ",s.tinyurl.short(url),"\n")
 
 shortenurl(url)
 
